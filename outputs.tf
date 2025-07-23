1f0f18f0449b3d1981f69dd3bc4d7b59bff3e999
Joseph Thomas <josephdthomas@google.com>
2025-07-23 22:19:27 +0000

    Create basic functional TF module
    Change-Id: Ia578895af4926fcb5af6fa62f0b93e5527d1b8be

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# This file defines the values that the module will return.
+
+output "output_value" {
+  description = "The value passed into the module."
+  value       = var.input_value
+}
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+# This file defines the input parameters for the module.
+
+variable "input_value" {
+  description = "A sample input value for the module."
+  type        = string
+  default     = "Hello, Terraform!"
+}
+
